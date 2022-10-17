7cdf15dc388e6da182c7c26d94ffb4138be1f5c6
Jiawei Zhou <zhou.jiaw@northeastern.edu>
2022-10-17 16:59:58 -0600

    ok

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main() {
-   // printf() displays the string inside quotation
-   printf("12+34=%d", 12+34);
+   int amount = 100;
+   int price = 0;
+   printf("enter price");
+   scanf("%d", &price);
+   printf("enter amount");
+   scanf("%d", &amount);
+   int change = amount - price;
+   printf("give you %d money. \n", change);
    return 0;
 }
